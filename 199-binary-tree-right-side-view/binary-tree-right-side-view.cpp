d9f71cf4a3ce11b71b69ccef2601b7fac31a9aa8
Ariza Wasim <arizawasim@gmail.com>
2026-07-31 17:18:36 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (63.93%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution { // left view hota tph first lete jab i == 0 hota
 public:
     vector<int> rightSideView(TreeNode* root) { //bfs karenge since level wise last ele
         vector<int> ans;
@@ -25,14 +25,13 @@ public:
                 TreeNode* node = q.front();
                 q.pop(); // pop karte jao jab tak last ele na aaye level ka
 
-                if(i == n - 1) //mila aakhri node of this level
+                if(i == n - 1) // i ab aakhri turn of level --> mila aakhri node of this level
                     ans.push_back(node->val); //ans mein dalo
 
                 if(node->left) q.push(node->left); // repeat for left child
                 if(node->right) q.push(node->right); // repeat for right child
             }
         }
-
         return ans; // ans bheja of all righties
     }
 };
