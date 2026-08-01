5d7e7770925f3c68339c8c6325b9e082863fb1e1
Ariza Wasim <arizawasim@gmail.com>
2026-08-01 18:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (85.12%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[21][21];
+
+    int solve(int i, int j, vector<int>& nums) {
+        if (i == j) return nums[i];
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int takeLeft = nums[i] - solve(i + 1, j, nums);
+        int takeRight = nums[j] - solve(i, j - 1, nums);
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        return solve(0, nums.size() - 1, nums) >= 0;
+    }
+};
