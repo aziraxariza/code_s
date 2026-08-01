ba5e4c46846a1eca558fc1f196afe9372f36bfbb
Ariza Wasim <arizawasim@gmail.com>
2026-08-01 19:51:06 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (32.30%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.cpp
@@ -14,9 +14,12 @@ public:
     TreeNode* searchBST(TreeNode* root, int val) {
         if(!root) return NULL;
 
-        if(root -> val == val) return root;
+        if(root->val == val)
+            return root;
 
-        if(val < root -> val) return searchBST(root -> left, val);
-        return searchBST(root -> right, val);
+        if(val < root->val)
+            return searchBST(root->left, val);
+
+        return searchBST(root->right, val);
     }
 };
