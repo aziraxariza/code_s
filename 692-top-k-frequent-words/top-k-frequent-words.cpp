43ce4cd306d4c0d60449ca18145c8ca026691fd2
Ariza Wasim <arizawasim@gmail.com>
2026-08-04 14:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (8.43%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,35 +1,60 @@
-struct Compare {
-        bool operator()(const pair<int, string> &a, const pair<int, string> &b){    
-            if(a.first==b.first){
-                return a.second<b.second; //priority given to b over a
-            } else {
-                return a.first>b.first;
-            }
+       /* BRUTE FORCE
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> mp; // string --> freq
+        for(string s : words){
+            mp[s]++; 
+        }
+
+        priority_queue<pair<int, string>> pq; // stores max to min string by freq
+        for(auto it : mp){
+            pq.push({it.second, it.first}); // (freq, string) pairwise
+
+        }
+
+        vector<string> ans;
+        for(int i = 0; i < k; i++){ // k times get top 
+            string w = pq.top().second;
+            pq.pop();
+            ans.push_back(w);
+        }
+        return ans; */
+
+class Solution {
+   public:
+
+    struct cmp{
+
+        bool operator()(pair<int,string>& a, pair<int,string>& b){
+
+            if(a.first == b.first) // agar dono words ki frequency same hai
+                return a.second > b.second; // lexicographically bada word lower priority paaye, taaki chhota word top par aaye BASICALLY IDHAR PRIORITY ASSIGN KAR RAHE HAI NA KI COMPARISON
+            // agar true aaya toh uska higher priority if false then opposite
+            return a.first < b.first; // jiski frequency kam hai uski priority kam hogi, max frequency top par aayegi
         }
     };
 
-class Solution {
-public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> m;
-        priority_queue<pair<int, string>, vector<pair<int, string>>, Compare> pq;
 
-        for(string w:words){
-            m[w]++;
-        }
+        unordered_map<string,int> mp; // word -> frequency
 
-        for(auto &[w, i]:m){
-            pq.push({i, w});
-            if(pq.size()>k){
-                pq.pop();
-            }
+        for(string s : words)
+            mp[s]++;// har word ki frequency count 
+
+        priority_queue< // ***
+            pair<int,string>,
+            vector<pair<int,string>>,
+            cmp
+        > pq;  // custom comparator wala max heap
+
+        for(auto &it : mp){
+            pq.push({it.second,it.first}); // (frequency, word) store 
         }
-        vector<string> res;
-        while(!pq.empty()){
-            res.push_back(pq.top().second);
-            pq.pop();
+        vector<string> ans;
+
+        while(k--){  // top k frequent words nikalne hain
+            ans.push_back(pq.top().second);// pair ka second is actual word
+            pq.pop(); // next pane ke liye now pop
         }
-        reverse(res.begin(), res.end());
-        return res;
+        return ans;
     }
 };
