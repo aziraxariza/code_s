e2e8efb49c531f5d6c08095567b283bf24651cf7
Ariza Wasim <arizawasim@gmail.com>
2026-08-08 18:39:32 +0530

    Time: 36 ms (11.55%) | Memory: 34.7 MB (87.05%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(); // rows
+        int n = mat[0].size(); // cols
+
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0)
+                    q.push({i,j}); // sabhi 0 source hain
+                else
+                    mat[i][j] = -1; // 1 ko unvisited mark karo
+            }
+        }
+
+        int dr[] = {1,-1,0,0};
+        int dc[] = {0,0,1,-1}; // 4 directions
+
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
+
+            for(int k = 0; k < 4; k++){
+                int nr = r + dr[k];
+                int nc = c + dc[k]; // agal bagal 
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n 
+                   && mat[nr][nc] == -1){
+
+                    mat[nr][nc] = mat[r][c] + 1; // distance = jisse bfs call usme +1
+                    q.push({nr,nc}); // next BFS level
+                }
+            }
+        }
+
+        return mat;
+    }
+};
