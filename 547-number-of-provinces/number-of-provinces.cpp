583ad5c53824fd9882792137cffa08d812577651
Ariza Wasim <arizawasim@gmail.com>
2026-08-09 23:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (71.68%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
-        vis[node] = 1;
 
-        for (int j = 0; j < isConnected.size(); j++) {
-            if (isConnected[node][j] == 1 && !vis[j]) {
-                dfs(j, isConnected, vis);
-            }
+    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
+        vis[node] = 1; // mark visited
+
+        for(int j = 0; j < isConnected.size(); j++) { // usse jude hue khojne ke liye adj matrix mein
+            if(isConnected[node][j] == 1 && !vis[j])
+                dfs(j, isConnected, vis); // usse jude hai toh dfs mein ghumo aur visited mark karo
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<int> vis(n, 0);
-        int provinces = 0;
+        vector<int> vis(n, 0); // made visited to track each node
 
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                dfs(i, isConnected, vis);
-                provinces++;
+        int province = 0;
+
+        for(int i = 0; i < n; i++) { // har ek node
+            if(!vis[i]) { // nahi kiya visit toh
+                province++; // naya province shuru
+                dfs(i, isConnected, vis); //isse connected
             }
         }
-
-        return provinces;
+        return province;
     }
 };
