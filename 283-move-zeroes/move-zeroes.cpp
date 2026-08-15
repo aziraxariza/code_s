3793b33e756d65dc65032a37cea44e2f7bacadab
Ariza Wasim <arizawasim@gmail.com>
2026-08-15 17:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -3,10 +3,10 @@ public:
     void moveZeroes(vector<int>& nums) {
         int j = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
+        for (int i = 0; i < nums.size(); i++) { // scan karenge array * j apne aap badhta chalega
+            if (nums[i] != 0) { // zero nahi hai
+                swap(nums[i], nums[j]); // toh swap with slow ptr value
+                j++; // slow aage badhaya
             } 
         }
     }
