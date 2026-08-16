1480dc688ab623532d2c352c6109847e82f591d1
Ariza Wasim <arizawasim@gmail.com>
2026-08-16 21:34:43 +0530

    Time: 2 ms (84.44%) | Memory: 11.3 MB (97.92%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int s_len = s.length();
-        int p_len = p.length();
-        
-        if(s.size() < p.size()) return {};
-        
-        vector<int> freq_p(26,0);
-        vector<int> window(26,0);
-        
-        //first window
-        for(int i=0;i<p_len;i++){
-            freq_p[p[i]-'a']++;
-            window[s[i]-'a']++;
-        }
-        
         vector<int> ans;
-        if(freq_p == window) ans.push_back(0);
-        
-        for(int i=p_len;i<s_len;i++){
-            window[s[i-p_len] - 'a']--;
-            window[s[i] - 'a']++;
-            
-            if(freq_p == window) ans.push_back(i-p_len+1);
+
+        if(s.size() < p.size()) return ans; // not possible to check
+
+        vector<int> freq_s(26, 0); // isme hum p ke size ke window acc chars daalke check karenge 1 by 1
+        vector<int> freq_p(26, 0); // saare p ke chars ki freq store
+
+        for(int i = 0; i < p.size(); i++){
+            freq_p[p[i] - 'a']++; // p ke chars stored
         }
+
+        for(int r = 0; r < s.size(); r++){ // p.size() is our window range
+            freq_s[s[r] - 'a']++;
+
+            if(r >= p.size()){
+                freq_s[s[r-p.size()] - 'a']--; // range se bada toh hatao window se
+            }
+            if(freq_s == freq_p) ans.push_back(r - p.size() + 1); // woh idx jisse ye anagram shuru hua
+        }
+
         return ans;
     }
 };
