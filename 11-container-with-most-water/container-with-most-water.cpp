e42dc589d8b2e1bfead214bd767d9c80b05f3b6a
Ariza Wasim <arizawasim@gmail.com>
2026-08-18 21:59:28 +0530

    Time: 1 ms (44.45%) | Memory: 62.8 MB (98.29%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int l = 0;
-        int r = height.size() - 1;
+        int l = 0; // start idx
+        int r = height.size() - 1; // last idx
+
         int ans = 0;
+        while(l < r){
+            int length = r-l; // total distance bw the two
+            int breadth = min(height[l], height[r]);
 
-        while (l < r) {
-            int area = min(height[l], height[r]) * (r - l);
-            ans = max(ans, area);
+            ans = max(ans, length*breadth);
 
-            if (height[l] < height[r]) // jiski height kum usse aage badhenge
+            if(height[l] > height[r]){
+                r--; // jo bada hai usko rakhenge aur uske opposite ko badhayenge
+            }
+            else{
                 l++;
-            else
-                r--;
+            }
         }
-
         return ans;
     }
 };
