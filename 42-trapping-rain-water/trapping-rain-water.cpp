9b24d6dbe4cc2e51b335b9e9cf7da4ed0f9f80dc
Ariza Wasim <arizawasim@gmail.com>
2026-08-20 21:07:22 +0530

    Time: 0 ms (100.00%) | Memory: 26 MB (77.72%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
+        int l = 0; // starting ptr
+        int r = height.size()-1; // end ptr
+        int leftMax = 0; // max height from left side
+        int rightMax = 0; 
 
-        int left = 0, right = n - 1;
-        int leftMax = 0, rightMax = 0;
-        int water = 0;
-
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    water += leftMax - height[left];
-
-                left++;
-            } else {
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    water += rightMax - height[right];
-
-                right--;
+        int ans = 0; // final answer
+        while(l < r){
+            if(height[l] < height[r]){// humesha chhoti height se process karenge
+                if(height[l] > leftMax){
+                    leftMax = height[l]; // biggest height from l as of now
+                }
+                else{ // agar leftMax  height se chhota hai toh uska height difference hoga water added
+                    ans += leftMax - height[l];
+                }
+                l++; // left se aage badhao for next
+            }
+            else{ // r par chhoti height udhar se process karo ab
+                if(height[r] > rightMax){
+                    rightMax = height[r]; // biggest height from r as of now
+                }
+                else{
+                    ans += rightMax - height[r];
+                }
+                r--; // peeche badhao right se
             }
         }
-
-        return water;
+        return ans; 
     }
 };
