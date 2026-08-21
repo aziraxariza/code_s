e45d35a5988f98cd2d66ff5990b4a4d370556c50
Ariza Wasim <arizawasim@gmail.com>
2026-08-21 17:03:07 +0530

    Time: 18 ms (97.96%) | Memory: 16.3 MB (70.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size(); // row col liya
-
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return; // base cases
-
-        grid[i][j] = '0'; // isko pani bana do
-
-        dfs(i + 1, j, grid); // all 4 dircns mein same karo for one continuos piece of land
-        dfs(i - 1, j, grid);
-        dfs(i, j + 1, grid);
-        dfs(i, j - 1, grid);
-    }
-
-    int numIslands(vector<vector<char>>& grid) {
+    void dfs(int i, int j, vector<vector<char>>& grid){
         int m = grid.size();
         int n = grid[0].size();
-        int islands = 0;
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == '0') return;
+        
+        grid[i][j] = '0'; // isko paani banaya to mark it as visited
+        
+        dfs(i+1, j, grid); // continuos piece of island ka land 4 dirns mein check
+        dfs(i-1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i, j-1, grid); // make agal bagal ka land as 0 if 1
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(); // no. of rows
+        int n = grid[0].size(); // cols
+        int islands = 0; 
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) { // traverse grid get 1s jo hai
-                if (grid[i][j] == '1') {
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
                     islands++; // island shuru
-                    dfs(i, j, grid); // ab hum agal bagal ko traverse karenge aur pani bana denge
+                    dfs(i, j, grid);
                 }
             }
         }
-
         return islands;
     }
 };
