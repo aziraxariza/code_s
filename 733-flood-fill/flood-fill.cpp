e89d92a3e6df535ee4e4b9922f53af19c0d1a6fe
Ariza Wasim <arizawasim@gmail.com>
2026-08-21 19:43:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (90.06%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:        
-    void dfs(int i, int j, vector<vector<int>>& image, int color, int beg, vector<vector<int>>& vis){
+    void dfs(int i, int j, vector<vector<int>>& image, int color, int ini){
         int m = image.size();
         int n = image[0].size();
+        if(i < 0 || i >= m || j < 0 || j >= n || image[i][j] != ini || image[i][j] == color) return; // agar out of bounds or not same as ini or alr colored
 
-        if(i < 0 || i >= m || j < 0 || j >= n || image[i][j] != beg || vis[i][j] == 1) return; // out of bounds to be updated
-
-        vis[i][j] = 1; // mark visited
-        image[i][j] = color; // update color
-
-        dfs(i+1, j, image, color, beg, vis);
-        dfs(i-1, j, image, color, beg, vis);
-        dfs(i, j+1, image, color, beg, vis);
-        dfs(i, j-1, image, color, beg, vis);
+        image[i][j] = color; // mark visited by painting as color
+        dfs(i+1, j, image, color, ini); // 4 dirxns
+        dfs(i-1, j, image, color, ini);
+        dfs(i, j+1, image, color, ini);
+        dfs(i, j-1, image, color, ini);
 
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int m = image.size(); // rows
+        int m = image.size(); // no. of rows
         int n = image[0].size(); // cols
 
-        int beg = image[sr][sc]; // save kiya jinhe change karenge
+        int ini = image[sr][sc]; // initial color
 
-        vector<vector<int>> vis(m, vector<int>(n, 0)); // to keep track of visited ones
-
-        dfs(sr, sc, image, color, beg, vis);// call dfs yaha se
-
+        dfs(sr, sc, image, color, ini); // yaha se shuruat dfs ko
+        
         return image;
     }
 };
