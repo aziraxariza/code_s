f492a51b5dacc5baf6c6937d4880bc83f429dc0b
Ariza Wasim <arizawasim@gmail.com>
2026-08-23 15:39:46 +0530

    Time: 12 ms (80.99%) | Memory: 34.5 MB (96.37%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,7 +4,7 @@ public:
         int m = mat.size(); // no. of rows
         int n = mat[0].size(); // cols
 
-        queue<pair<int, int>> q; // isme sare 1 jayenge 
+        queue<pair<int, int>> q; // isme sare jayenge but pehle saare 0s jayenge
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0){
@@ -23,14 +23,16 @@ public:
             auto [r, c] = q.front(); // isko uthaya
             q.pop();
 
-            for(int k = 0; k < 4; k++){
+            for(int k = 0; k < 4; k++){ 
                 int nr = r + dr[k];
                 int nc = c + dc[k]; 
 
+                // agalbagal ke -1 ke dist update r c ki madad se
                 if(nr >= 0 && nr < m && nc >= 0 && nc < n && mat[nr][nc] == -1){
                     // sirf iske adjacent -1 i.e unvis 1 walo ki update karenge dist
+
                     mat[nr][nc] = mat[r][c] + 1; // jiska adj hai uski 0 se dist. +1
-                    q.push({nr, nc}); // ab isse jude -1 iska 0 se dist +1 honge 
+                    q.push({nr, nc}); // ab isse jude -1 ke 0 se dist iskadist+1 honge 
                 }
             }
         }
