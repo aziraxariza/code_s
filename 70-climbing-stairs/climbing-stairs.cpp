9c0a5c65019aa6653cc46843f851ec886de0c707
Ariza Wasim <arizawasim@gmail.com>
2026-08-24 22:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if(n == 0 || n == 1) return 1;
-        vector<int> dp(n+1);
-        dp[0] = dp[1] = 1;
 
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+    int climbStairs(int n) {
+        if(n <= 2) return n; // base case
+        vector<int> dp(n+1); // tabulation
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2]; // fibonacci series follow kr rha hai ye qosn
         }
         return dp[n];
     }
