7690e499e1ffb7687bc0e13c7ff02a9354c7f96d
Ariza Wasim <arizawasim@gmail.com>
2026-08-25 21:53:38 +0530

    Time: 4 ms (23.12%) | Memory: 9.4 MB (58.57%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+
+        unordered_map<char, char> mp1;
+        unordered_map<char, char> mp2;
+
+        for(int i = 0; i < s.size(); i++){
+            char a = s[i];
+            char b = t[i];
+
+            if(mp1.count(a) && mp1[a] != b) return false; // mp1 mein a hai but mp[a]-->b nhi hai
+            if(mp2.count(b) && mp2[b] != a) return false;
+
+            mp1[a] = b;
+            mp2[b] = a;
+        }
+        return true;
+    }
+};
