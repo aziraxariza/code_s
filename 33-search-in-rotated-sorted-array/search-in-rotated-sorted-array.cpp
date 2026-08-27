c5ddc55d72d8cc8eab4ae1501189e26b623ef475
Ariza Wasim <arizawasim@gmail.com>
2026-08-27 15:48:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (69.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -5,21 +5,22 @@ public:
         int hi = nums.size() - 1;
 
         while(lo <= hi){
-            int mid = lo + (hi - lo) / 2;
+            int mid = lo + (hi-lo)/2;
 
-            if(nums[mid] == target) return mid; // mid par mil gaya
+            if(nums[mid] == target) return mid; // mil gaya return idx
 
-            if(nums[lo] <= nums[mid]){ // agar left side sorted hai i.e left kum hai mid se
-                if(nums[lo] <= target && target < nums[mid]){ // target lo aur mid ke beech
+            if(nums[lo] <= nums[mid]){ // target left sorted range mein hai
+                if(nums[lo] <= target && target < nums[mid]){
                     hi = mid - 1;
                 }
                 else{
-                    lo = mid + 1; // warna right side pe hai mid ke
+                    lo = mid + 1;
                 }
             }
-            else{ // ab humara right side sorted hai
-                if(nums[hi] >= target && target > nums[mid]){ // target mid aur hi ke beech hai
-                    lo = mid + 1; 
+
+            else{ // target right sorted range mein hai
+                if(nums[mid] < target && target <= nums[hi]){
+                    lo = mid + 1;
                 }
                 else{
                     hi = mid - 1;
