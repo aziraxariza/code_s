71214507aa2c75322cf1d6d4afebaeea9f51d1cc
Ariza Wasim <arizawasim@gmail.com>
2026-08-28 15:34:22 +0530

    Time: 24 ms (41.75%) | Memory: 70.6 MB (48.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,39 +2,38 @@ class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
+        if((long long)m*k > n) return -1; // required flowers > garden mein flowers
 
-        if((long long)m * k > n) return -1; // not enough flowers
+        int lo = *min_element(bloomDay.begin(), bloomDay.end()); // sabse kum kitne din mein uga
+        int hi = *max_element(bloomDay.begin(), bloomDay.end()); // max kitne din mein uga
 
-        int lo = *min_element(bloomDay.begin(), bloomDay.end());
-        int hi = *max_element(bloomDay.begin(), bloomDay.end());
+        while(lo < hi){
+            int mid = lo + (hi-lo)/2; // mid as in itne din tak le kar chalo wait time to get all bouquets
 
-        while(lo <= hi){
-            int mid = lo + (hi - lo) / 2;
-
-            int flowers = 0;
-            int bouquets = 0;
+            int flowers = 0; // is  mid ke acc kitne flowers hue
+            int bouquets = 0; // kitne bouquets complete
 
             for(int x : bloomDay){
-                if(x <= mid){
-                    flowers++;
+                if(x <= mid){ // mid ya usse kum din mein ug gaya
+                    flowers++; // ek flower lelo
 
                     if(flowers == k){
-                        bouquets++;
-                        flowers = 0;
+                        bouquets++; // ek bouquet complete
+                        flowers = 0; // new bouquet ke liye val 0 set krdo wapas
                     }
                 }
-                else{
-                    flowers = 0;
+                else{ // mid se zyada  din mein uga
+                    flowers = 0; // set flowers to 0 kyuki hume adj flowers chahiye ek bouquet mein
                 }
             }
 
             if(bouquets >= m){
-                hi = mid - 1; // mid works, try fewer days
+                hi = mid; // mid se kum days check karo ki suffice kr rhe hai 
             }
             else{
-                lo = mid + 1; // need more days
+                lo = mid+1; // zyada din chahiye since bouquets nahi bane saare
             }
         }
-        return lo;
+        return lo; // kum se kum din jisme sare bouquet bn jaye
     }
 };
