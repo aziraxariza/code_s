ea567f2fa9c216a9f516c425a5595f49e3560a44
Ariza Wasim <arizawasim@gmail.com>
2026-08-28 21:51:16 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (56.22%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int l = 0; // starting ptr
-        int r = height.size()-1; // end ptr
-        int leftMax = 0; // max height from left side
-        int rightMax = 0; 
+        int left = 0;
+        int right = height.size()-1;
+        int leftMax = 0;
+        int rightMax = 0; // max height from left and right
 
-        int ans = 0; // final answer
-        while(l < r){
-            if(height[l] < height[r]){// humesha chhoti height se process karenge
-                if(height[l] > leftMax){
-                    leftMax = height[l]; // biggest height from l as of now
-                }
-                else{ // agar leftMax  height se chhota hai toh uska height difference hoga water added
-                    ans += leftMax - height[l];
-                }
-                l++; // left se aage badhao for next
-            }
-            else{ // r par chhoti height udhar se process karo ab
-                if(height[r] > rightMax){
-                    rightMax = height[r]; // biggest height from r as of now
+        int ans = 0;
+        while(left < right){
+
+            if(height[left] <= height[right]){ // jis side height kum udhar se calc shuru
+                if(height[left] > leftMax){ // isko leftMax banao
+                    leftMax = height[left];
                 }
                 else{
-                    ans += rightMax - height[r];
+                    ans += leftMax - height[left]; // add diff to get unit of water
                 }
-                r--; // peeche badhao right se
+                left++; // left se aage badhao
             }
+
+            else{
+                if(height[right] > rightMax){ // isko leftMax banao
+                    rightMax = height[right];
+                }
+                else{
+                    ans += rightMax - height[right]; // add diff to get unit of water
+                }
+                right--; // right se peeche
+            }
+
         }
-        return ans; 
+        return ans;
     }
 };
