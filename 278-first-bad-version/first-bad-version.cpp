694dc6ef431c12702d702f1c4594b2b466c7eda1
Ariza Wasim <arizawasim@gmail.com>
2026-08-30 16:39:22 +0530

    Time: 3 ms (16.98%) | Memory: 8 MB (5.12%) - LeetSync

--- 278-first-bad-version/first-bad-version.cpp
+++ 278-first-bad-version/first-bad-version.cpp
@@ -7,10 +7,10 @@ public:
         int lo = 1;
         int hi = n;
 
-        while(lo <= hi){
+        while(lo < hi){
             int mid = lo + (hi-lo)/2; // mid version dekha
             if(isBadVersion(mid)){
-                hi = mid-1; // isse chhota version dhundho
+                hi = mid; // isse chhota version dhundho
             }
             else{
                 lo = mid+1; // isse bada version kharab hoga
