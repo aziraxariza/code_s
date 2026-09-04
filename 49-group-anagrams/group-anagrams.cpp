734af5fe5cf5ba56606fb9a42c7cea686f1e5265
Ariza Wasim <arizawasim@gmail.com>
2026-09-04 15:14:44 +0530

    Time: 10 ms (93.03%) | Memory: 25.2 MB (63.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> check;
+        unordered_map<string, vector<string>> mp; // mp[key] = {x, y..}
 
-        for(string s : strs){
-            string s2 = s;
-            sort(s2.begin(), s2.end());
-            check.push_back({s2, s}); // s aur uske sorted ko check mein dala
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); // sort key
+
+            mp[key].push_back(s); // og word mp[key] mein
         }
-        sort(check.begin(), check.end()); // sort check for anagrams to be beside each othr
 
-        int l = 0;
-        vector<string> same;
         vector<vector<string>> ans;
-
-        same.push_back(check[l][1]); 
-
-        for(int r = 1; r < check.size(); r++){
-            if(check[r][0]==check[l][0]){
-                same.push_back(check[r][1]); // same hai l se uske sath dalo
-            }
-            else{
-                ans.push_back(same); // alag hua jaise waise pehle walo ko dala as 1 level
-                same.clear(); // khali kiya same ko
-                l = r; // new checked posn
-                same.push_back(check[r][1]);
-            }
+        for (auto &it : mp) {
+            ans.push_back(it.second); // ans mein vals dalo 
         }
-        ans.push_back(same); // last group ko push to ans kyuki woh sirf same mein reh jayenge upar wale if else se
 
         return ans;
     }
