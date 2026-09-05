fd30d39b69221a8943b195b3692594ded921c395
Ariza Wasim <arizawasim@gmail.com>
2026-09-05 19:35:34 +0530

    Time: 7 ms (77.75%) | Memory: 202.5 MB (96.72%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -6,13 +6,13 @@ public:
         int mint=INT_MAX;
         for(int i=nums.size()-1;i>=0;i--){
             if(nums[i]<mint) mint = nums[i];
-            mini[i] = mint;
+            mini[i] = mint; // peeche se har ek idx se end tak ka mini store
         }
 
         int maxt=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]>maxt) maxt = nums[i];
-            if(maxt-mini[i]<=k) return i;
+            if(maxt-mini[i]<=k) return i; // iss tak ka ka max - isse lekar end tak ka mini
         }
         return -1;
     }
