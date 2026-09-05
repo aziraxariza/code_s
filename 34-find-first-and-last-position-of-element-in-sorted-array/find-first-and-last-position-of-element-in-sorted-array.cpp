56a59caee80c88de71e57283b6218c0abcd465dd
Ariza Wasim <arizawasim@gmail.com>
2026-09-05 19:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (53.61%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,33 +4,40 @@ public:
         int lo = 0;
         int hi = nums.size()-1;
 
-        vector<int> ans(2, -1); // ans vector
+        vector<int> ans = {-1, -1}; // final posns of target in nums
 
-        while(lo <= hi){ // BS1 left most target eqivalen ke liye
-            int mid = lo + (hi-lo)/2; //mid nikala posn
-
-            if(nums[mid] == target){
-                ans[0] = mid;
-                hi = mid-1; // aur left mein khojo wapas bs ek posn peeche tak karke
-            }
-            else if(nums[mid] > target) hi = mid-1;
-            else lo = mid+1; // lo hi change accordingly
-        }
-
-        lo = 0; // ab right side bs to find more targets on right side
-        hi = nums.size()-1;
-        
-        while(lo <= hi){ // first mid same aayegi dono ki.. next se updated bigger mid posn wali aayengi
-            int mid = lo + (hi-lo)/2; //mid nikala posn
+        while(lo <= hi){ // BS1 leftmost target ki index
+            int mid = lo + (hi-lo)/2;
 
             if(nums[mid] == target){
-                ans[1] = mid;
-                lo = mid+1; // aur right mein khojo wapas bs ek posn aage se
+                ans[0] = mid; // left most ka index ye set kiya
+                hi = mid-1; // isse chhote idx par target dhundho
             }
-            else if(nums[mid] > target) hi = mid-1;
-            else lo = mid+1; // lo hi change accordingly
+            else if(nums[mid] < target){
+                lo = mid+1; // mid ke aage dhundho 
+            }
+            else{
+                hi = mid-1; // mid se chhota hai
+            }
         }
 
+        lo = 0;
+        hi = nums.size() - 1; //BS2 ke liye wapas redeclare
+
+        while(lo <= hi){ // BS2 rightmost target ki index
+            int mid = lo + (hi-lo)/2;
+
+            if(nums[mid] == target){
+                ans[1] = mid; // right most ka index ye set kiya
+                lo = mid+1; // isse bade idx par target dhundho
+            }
+            else if(nums[mid] < target){
+                lo = mid+1; // mid ke aage dhundho 
+            }
+            else{
+                hi = mid-1; // mid se chhota hai
+            }
+        }
         return ans;
     }
 };
