5141028fe5fc8a2e3f47436d6c8a36fa15fecb7f
Ariza Wasim <arizawasim@gmail.com>
2026-09-06 21:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (52.53%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
+        int slow = 0;
 
-        int j = 0;
-
-        for(int i = 1; i < n; i++){
-            if(nums[i] != nums[j]){ // dono alag hai toh
-                j++; // j badhao
-                nums[j] = nums[i]; // j ki posn par ith daal do 
+        for(int fast = 1; fast < nums.size(); fast++){
+            if(nums[fast] != nums[slow]){
+                slow++; // agar same nahi toh slow ko ek badha kar uss posn par nums[j] val daal do 
+                nums[slow] = nums[fast];
+                // agar dono same hote tohslow wahi ruka rehta aur jaise hi koi different aata woh slow + 1th posn par update karta
             }
         }
-
-        return j + 1; // j is idx so idx+1 eles unique hai
+        return slow + 1; // slow jis idx par hai usse ek zyada unique numbers hai
     }
 };
+
+/*  visualise
+nums = [1,2,3,2,3]
+       ↑─────↑
+       first k=3 */ 
