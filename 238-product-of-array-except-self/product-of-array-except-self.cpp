9af9fd4c50ea04074eb85b4f437cadd2e7459ead
Ariza Wasim <arizawasim@gmail.com>
2026-09-06 17:19:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (81.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n, 1); // sab abhi 1 hai
+        vector<int> ans(n, 1); // final ans
 
-        for(int i = 1; i < n; i++){ // prefix product
-            ans[i] = ans[i-1] * nums[i-1]; // khudko chodke peeche tak ke prdt aur nums mein khudke peeche ka kyuki peeche wale mein woh misisng hai
-        }
-        
-        int suffix = 1; // suffix prdt
-        for(int j = n-1; j >= 0; j--){
-            ans[j] *= suffix; // isko first suffix se multiply
-            suffix *= nums[j]; // make curr right as nums[j]
+        for(int i = 1; i < n; i++){ // prefix way
+            ans[i] = ans[i-1] * nums[i-1]; //peeche wale tak ka ans * peeche wale ki val
         }
 
+        int suffix = 1;
+        for(int j = n-1; j >= 0; j--){ // suffix way
+            ans[j] *= suffix;
+            suffix *= nums[j]; // update suffix by * last val
+        }
         return ans;
     }
 };
