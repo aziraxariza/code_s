c98133ed8816c64198fd5161b947a044230ac30f
Ariza Wasim <arizawasim@gmail.com>
2026-09-07 21:13:15 +0530

    Time: 4 ms (52.81%) | Memory: 9.9 MB (89.46%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s2.size() < s1.size()) return false; // possible hi nahi hai
+        if(s2.size() < s1.size()) return false;
 
-        vector<int> freq_s1(26, 0);// inke chars ki freq rakho
-        vector<int> freq_s2(26, 0);  // isme hum sirf range walo ki freq check karke vectors match karenge
+        vector<int> freq_s1(26, 0);
+        vector<int> freq_s2(26, 0);
 
         for(char x : s1){
-            freq_s1[x - 'a']++; // s1 unchanged rahega
+            freq_s1[x-'a']++; // s1 ke store kr lo saare chars ki freq
         }
 
-        int l = 0;
-        int k = s1.size();
-        for(int r = 0; r < s2.size(); r++){ // s2 ko scan
-            freq_s2[s2[r] - 'a']++; // iski freq badhayi
+        int k = s1.size(); // s1 ka size
 
-            if(r >= k){ // out of window gaye
-                freq_s2[s2[r-k] - 'a']--;
+        for(int r = 0; r < s2.size(); r++){
+            freq_s2[s2[r]-'a']++; // iski freq store
+
+            if(r >= k){ // idx is now >= k toh peeche se ek ek hatyenge jab hum ek naya char add karenge
+                freq_s2[s2[r-k]-'a']--; // k posn peeche wale ki freq reduce
             }
-            if(freq_s1 == freq_s2) return true; // dono ke k chars same freq ke hai
+
+            if(freq_s1 == freq_s2) return true; // same chars in window as s1
         }
         return false;
     }
