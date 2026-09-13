9c6065f616773380c5efb58e9aa7c1bcfb8158d7
Ariza Wasim <arizawasim@gmail.com>
2026-09-13 19:10:35 +0530

    Time: 11 ms (87.24%) | Memory: 23.4 MB (91.39%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n = grid.size();
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1; // pehle hi impossible
+        int n = grid.size(); // no. of rows and cols
 
-        queue<pair<int, int>> q; //bfs q
-        int dist = 1; // initial dist
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1; // not possible
 
-        q.push({0,0}); // starting posn
-        grid[0][0] = 1; // visited mark kiya
+        int dr[] = {1,1,1,-1,-1,-1,0,0}; // 8 dirxns ke liye
+        int dc[] = {-1,0,1,-1,0,1,-1,1}; 
 
-        int dr[] = {1,-1,0,0,-1,-1,1,1}; // for 8 dirxns
-        int dc[] = {0,0,1,-1,-1,1,-1,1};
+        int dist = 1; // initially dist 1 lekar chalo
 
+        queue<pair<int, int>> q; // {row, col}
+        q.push({0, 0}); // first cell dalo 
 
         while(!q.empty()){
-            int sz = q.size();
+            int sz = q.size(); // ek level ke cells same dist par process honge
             while(sz--){
-                auto [r, c] = q.front(); // get posn
+                auto [r, c] = q.front();
                 q.pop();
 
-                if(r == n-1 && c == n-1) return dist; // end takk pohoch gaye **
+                if(r == n-1 && c == n-1) return dist; // end tak aa gaye
 
                 for(int k = 0; k < 8; k++){
-                    int nr = r + dr[k]; // get posn of agal bagal 
-                    int nc = c + dc[k]; 
+                    int nr = r + dr[k];
+                    int nc = c + dc[k]; // 8 adj cell ke liye
 
                     if(nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0){
-                        grid[nr][nc] = 1; // mark visited
-                        q.push({nr,nc});
+                        grid[nr][nc] = 1; // mark vis
+                        q.push({nr, nc}); // q mein dalo isko
                     }
                 }
             }
-            dist++; // entire level ke baad dist + hoga
+            dist++; // ek level ke baad dist badhaya
         }
         return -1;
     }
