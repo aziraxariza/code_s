00e2ea61acf641d3542a85bca7b62d86fe0d9761
Ariza Wasim <arizawasim@gmail.com>
2026-09-16 16:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (42.30%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,34 +2,33 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int l = 0;
-        int r = height.size()-1; // idx of left and right
-        int leftMax = 0; 
-        int rightMax = 0;// max height from l and r side
+        int r = height.size()-1; // idx of l and r initially
+        int leftMax = 0;
+        int rightMax = 0; // height of biggest heights from l and r side
 
         int ans = 0; // final ans
-        while(l < r){
 
-            // JO CHHOTI HEIGHT UDHAR SE PROCESS KARENGE ****
-            if(height[l] <= height[r]){ 
-                if(height[l] > leftMax){
-                    leftMax = height[l]; // update bigger height from left side
+        while(l <= r){
+            if(height[l] <= height[r]){
+                if(height[l] > leftMax){// chhoti height se karenge humesa
+                    leftMax = height[l]; // l wala bada tha zayada so usko leftMax banaya 
                 }
                 else{
-                    ans += leftMax - height[l]; // dono ka diff is water bw them
+                    ans += leftMax-height[l]; // dono ke beech ka diff add to water
                 }
-                l++; // next l 
+                l++; // move aage
             }
 
             else{
                 if(height[r] > rightMax){
-                    rightMax = height[r]; 
+                    rightMax = height[r];
                 }
                 else{
-                    ans += rightMax - height[r]; 
+                    ans += rightMax-height[r];
                 }
-                r--; // next r
+                r--;
             }
         }
-        return ans;
+        return ans; // O(n)
     }
 };
