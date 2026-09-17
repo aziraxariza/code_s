1956d3675871e84de6ab5c6b46400ccd499f6368
Ariza Wasim <arizawasim@gmail.com>
2026-09-17 16:17:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (14.83%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,27 +1,27 @@
-class Solution {
+class Solution { //nums1 walo ke liye nge nikalo nums2 se
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st; // nums2 ke elements store karega peeche se
-        unordered_map<int, int> nxt; // ele --> nge se map karne ke liye; nums2 ke saaro ke nge map karlo nums1 subset hai toh req. hum nikal lenge  map se
+        stack<int> st; //nums2 ke eles peeche se jayenge
+        unordered_map<int, int> mp; // sab ele -> uska nge // mp[ele] = nge
 
-        for(int i = nums2.size()-1; i >= 0; i--){ // right to left traverse agar right se ele chahiye
+        for(int i = nums2.size()-1; i >= 0; i--){
 
-            while(!st.empty() && st.top() < nums2[i]){ // top chhota hai nums2[i] se
-                st.pop(); // nikalte raho jab tak bada na aaye cur se
+            while(!st.empty() && st.top() < nums2[i]){
+                st.pop(); // didnt clear the nge criteria
             }
-            if(st.empty()){ // st khaali hai yaani curr se bada nahi mila right side koi
-                nxt[nums2[i]] = -1;
+            if(st.empty()){
+                mp[nums2[i]] = -1; // has no nge
             }
             else{
-                nxt[nums2[i]] = st.top(); // cur ke right mein ye sabse paas mein bada hai
+                mp[nums2[i]] = st.top(); // nge mil gaya
             }
-            st.push(nums2[i]); // curr ko ab add in st so that ye right mein aaye curr - 1 ele ke liye
-        }
-        vector<int> ans; // final ans
-        for(int x : nums1){ // nums1 ke eles ke sirf nge chahiye
-            ans.push_back(nxt[x]); // unhe hum nxt se extract karenge
+            st.push(nums2[i]); // push to st
         }
 
+        vector<int> ans; // ans for each x in nums1
+        for(int x : nums1){ // sirf nums1 ke nums ki nge chahiye
+            ans.push_back(mp[x]);
+        }
         return ans;
     }
 };
