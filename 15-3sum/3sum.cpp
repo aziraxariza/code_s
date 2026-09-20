8550d9692bac9f54bc207f2f77a4f4f30c334be9
Ariza Wasim <arizawasim@gmail.com>
2026-09-20 15:11:09 +0530

    Time: 50 ms (44.71%) | Memory: 29.1 MB (45.82%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) { // fix one and uske aage se 2 ptr
-        vector<vector<int>> ans; // final ans;
         int n = nums.size();
+        vector<vector<int>> ans; // final ans;
 
-        sort(nums.begin(), nums.end()); // apni value ke hisab se sort ho gaye
+        sort(nums.begin(), nums.end()); // sort arr
 
-        for(int i = 0; i < n; i++){ // ith ko fix rakhenge as 1st 
-            if(i > 0 && nums[i-1] == nums[i]){
-                continue; // duplicate ith ke sath wapas repeated aa sakti hai answers
-            }
-            int first = nums[i];
+        for(int i = 0; i < n; i++){// isse hum traverse karenge each ele as 1st no.
+            if(i > 0 && nums[i] == nums[i-1]) continue; // skip duplicate values
+
             int l = i+1;
-            int r = n-1; // got our 2 pointrs
+            int r = n-1; // left and right ptrs
 
             while(l < r){
-                if(first+nums[l]+nums[r] == 0){
-                    ans.push_back({first, nums[l], nums[r]}); // mil gaya combo
 
-                    while(l<r && nums[l+1]==nums[l]){
-                        l++; // l ke repeated vals skip
-                    }
-                    while(l<r && nums[r-1]==nums[r]){
-                        r--; // r ke repeated vals skip
-                    }
+                if(nums[i]+nums[l]+nums[r] == 0){
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while(l < r && nums[l+1] == nums[l]) l++; // skip duplicates
+                    while(l < r && nums[r-1] == nums[r]) r--;
+
                     l++;
-                    r--; // aur check karo with this i as first
+                    r--; // check for more ans
                 }
-                
-                else if(first+nums[l]+nums[r] < 0){
-                    l++; // l wale se thodi badi val dekho
+                else if(nums[i]+nums[l]+nums[r] < 0){
+                    l++;
                 }
                 else{
-                    r--; // thodi chhoti dhundho r se
+                    r--;
                 }
             }
         }
