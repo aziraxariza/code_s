de92bf37e116dd155521994f90b025b45af191b4
Ariza Wasim <arizawasim@gmail.com>
2026-09-20 14:39:13 +0530

    Time: 2 ms (19.75%) | Memory: 10 MB (8.02%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -15,3 +15,17 @@ public:
         return ans;
     }
 };
+
+/*
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            n+=(i+1)*(26-(s[i]-'a'));
+        }
+        return n;
+    }
+};
+*/
