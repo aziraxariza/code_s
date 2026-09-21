e57579f327f0c0b71923cb606ab2f6acbbd235dc
Ariza Wasim <arizawasim@gmail.com>
2026-09-21 16:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (59.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> productExceptSelf(vector<int>& nums){
         int n = nums.size();
-        vector<int> ans(n, 1); // final ans
+        vector<int> ans(n, 1); // final ans hoga
 
-        for(int i = 1; i < n; i++){ // prefix way
-            ans[i] = ans[i-1] * nums[i-1]; //peeche wale tak ka ans * peeche wale ki val
+        // left side se i.e prefix 
+        for(int i = 1; i < n; i++){
+            ans[i] = nums[i-1] * ans[i-1]; // last val * peeche tak ka ans 
         }
 
-        int suffix = 1;
-        for(int j = n-1; j >= 0; j--){ // suffix way
-            ans[j] *= suffix;
-            suffix *= nums[j]; // update suffix by * last val
+        int suffix = 1; // right side se peeche
+        for(int i = n-1; i >= 0; i--){
+            ans[i] *= suffix; 
+            suffix *= nums[i]; // update suffix nums[i] ki val se
         }
+
         return ans;
     }
 };
