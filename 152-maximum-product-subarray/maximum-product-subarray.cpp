82a50feca9aacf980379fbb4885287bde0dbbb76
Ariza Wasim <arizawasim@gmail.com>
2026-09-21 17:08:14 +0530

    Time: 2 ms (15.74%) | Memory: 17.8 MB (59.98%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int curMax = nums[0]; // abhi tak ka max prdt
+        int curMin = nums[0]; // abhi tak ka min prdt
+
+        int ans = nums[0]; // initially ans
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < 1){ // negative val ke liye max aur min swap
+                swap(curMax, curMin); 
+            }
+            curMax = max(nums[i], curMax*nums[i]);
+            curMin = min(nums[i], curMin*nums[i]);
+
+            ans = max(ans, curMax);
+        }
+        return ans;
+    }
+};
