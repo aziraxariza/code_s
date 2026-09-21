314b58c596a4ef83707eaa29b8e6c29153fe3bf3
Ariza Wasim <arizawasim@gmail.com>
2026-09-21 17:34:01 +0530

    Time: 23 ms (51.48%) | Memory: 102.9 MB (79.09%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int n = temperatures.size();
+        vector<int> ans(n, 0); // for all temps
 
-        vector<int> ans(n, 0);
-        stack<int> st; // stores indices
-
-        for(int i = n - 1; i >= 0; i--) {
+        stack<int> st; // mono stack for idx 
+        
+        for(int i = n-1; i >= 0; i--){
             while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
-                st.pop(); // iss idx ki value kisi kaam ki nahi
+                st.pop(); // remove from stack
             }
-
             if(!st.empty()){
-                ans[i] = st.top() - i; // dist bw dono
+                ans[i] = st.top()-i; // dist
             }
-            st.push(i); // for all idxs
-        }
 
+            st.push(i); // push idx to st
+        }
         return ans;
     }
 };
