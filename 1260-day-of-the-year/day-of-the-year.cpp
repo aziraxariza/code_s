6d7efbbabaf0e9b3eca7b1c4061a9beca05acf24
Ariza Wasim <arizawasim@gmail.com>
2026-09-23 19:40:23 +0530

    Time: 8 ms (39.86%) | Memory: 12 MB (80.71%) - LeetSync

--- 1260-day-of-the-year/day-of-the-year.cpp
+++ 1260-day-of-the-year/day-of-the-year.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int dayOfYear(string date) {
-        vector days{31,28,31,30,31,30,31,31,30,31,30,31};
+        vector<int> days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        int y = stoi(date.substr(0,4));
-        if((y%4 == 0 && y%100 != 0) || (y%400 == 0)){
-            days[1] = 29;
+        int yr = stoi(date.substr(0,4));
+        if(yr%4 == 0 && yr%100 != 0 || yr%400 == 0){
+            days[1] = 29; // leap year
         }
 
-        int month = stoi(date.substr(5,2));
-        int ans = stoi(date.substr(8,2));
+        int ans = 0;
 
-        for(int i = 0; i < month-1; ++i){
+        int month = stoi(date.substr(5,2)); // kaunsa mahina
+        int addDays = stoi(date.substr(8,2));
+
+        if(month == 1) return addDays; // for jan
+        for(int i = 0; i < month-1; i++){
             ans += days[i];
         }
+        ans += addDays;
+
         return ans;
     }
 };
