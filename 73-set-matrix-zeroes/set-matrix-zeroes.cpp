8f3d7d349d7f99a0f9eefe7d1a3aa8ffe0e38d22
Ariza Wasim <arizawasim@gmail.com>
2026-09-23 20:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (82.79%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,6 +3,35 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
+
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1; // iss row mein 0
+                    col[j] = 1; // iss col mein zero
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row[i] || col[j]){ // iske row col mein koi zero tha
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+/*
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
         
         queue<pair<int, int>> q; // {i, j} of 0s
         for(int i = 0; i < m; i++){
@@ -26,3 +55,4 @@ public:
         }
     }
 };
+*/
