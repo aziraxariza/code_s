86ac8047d6f1dfcc33d11fc98fef8548dbf11649
Ariza Wasim <arizawasim@gmail.com>
2026-09-24 15:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (26.97%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,40 +11,30 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (!list1) return list2;
-        if (!list2) return list1; // koi ek exist nahi krti toh dusra return
+        if(!list1) return list2;
+        if(!list2) return list1; // agar koi ek exist nhi krta
 
-        ListNode* ans =  NULL; // initialized null node
-
-        ListNode* temp1 = list1; //temp1 and 2 with ptrs to resp list
+        ListNode* temp1 = list1;
         ListNode* temp2 = list2;
 
-        if(list1->val < list2->val){ // jo chhoti woh starting node in ans
-            ans = list1;
-            temp1 = temp1->next; // aage badhaya le kar
-        }
-        else{
-            ans = list2;
-            temp2 = temp2->next;
-        }
-        ListNode* head = ans; // ans ptr ko head banaya
+        ListNode* dummy = new ListNode(0);
+        ListNode* ans = dummy;
 
-        while(temp1 && temp2){ //dono mein kuch hai
-
-            if(temp1->val < temp2->val){ // jo chhota woh pehle
-                ans->next = temp1; // ab ye aayega
-                temp1 = temp1->next; //aage badhao
-                ans = ans->next; // aage badhao
+        while(temp1 && temp2){ // jab tak dono ke nodes hai
+            if(temp1->val < temp2->val){ // jiski chhoti val usko next node liya
+                ans->next = temp1;
+                temp1 = temp1->next; // next node of temp pr jao
             }
             else{
                 ans->next = temp2;
                 temp2 = temp2->next;
-                ans = ans->next;
             }
+            ans = ans->next; // ans aage badho
         }
-        if(temp1) ans->next = temp1;// agar kisiki nodes bach gayi toh ek ptr se sari attach ho jayengi
-        if(temp2) ans->next = temp2;
 
-        return head; //ptr to ans ka start
+        if(temp1) ans->next = temp1; // jiske nodes bach gaye
+        else ans->next = temp2;
+
+        return dummy->next;
     }
 };
